b2d6960c5aa8f025a66b34b06b454ee8f3695c12
Arthur <lobanovartur@gmail.com>
2022-05-08 02:32:52 +0300

    vai navorotil

--- cont/scripts/script.js
+++ cont/scripts/script.js
@@ -1,35 +1,57 @@
-$('#examples-carousel').slick({
-  dots: true,
-  arrows: false,
-  autoplay: true,
-  autoplaySpeed: 3000
+$(document).ready(function () {
+  $('#examples-carousel').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    touchMove: true,
+  })
+
+  let showPopupBtn = $('.show-popup')
+  let formPopup = $('#form-popup')
+  let form = formPopup.find('form')
+  let inputs = form.find('input')
+  let phoneInput = $('#phoneInput')
+
+  phoneInput.inputmask('+38(099)-999-99-99')
+
+  showPopupBtn.click(showPopup)
+  formPopup.click(hidePopup)
+  form.click((event) => {
+    event.stopPropagation()
+  })
+
+
+  function showPopup() {
+    formPopup.addClass('visible')
+    phoneInput.focus()
+  }
+
+  function hidePopup() {
+    formPopup.removeClass('visible')
+    formPopup.one('transitionend', () => {
+      inputs.val('')
+    })
+  }
+
+  let mail = 'lobanovartur@gmail.com, autoideakiev@gmail.com'
+  form.submit(function (event) {
+    event.preventDefault()
+    let to = '&to=' + mail
+    let form_data = $(this).serialize() + to
+    console.log(form_data)
+    $.ajax({
+      type: 'POST',
+      url: 'send.php',
+      data: form_data,
+      success: function () {
+        hidePopup()
+      },
+    })
+  })
 })
 
-$('a[href*="#"]')
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function (event) {
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash)
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
-      if (target.length) {
-        event.preventDefault()
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function () {
-          var $target = $(target)
-          $target.focus()
-          if ($target.is(':focus')) {
-            return false
-          } else {
-            $target.attr('tabindex', '-1')
-            $target.focus()
-          }
-        })
-      }
-    }
-  })
+
+
+
+
